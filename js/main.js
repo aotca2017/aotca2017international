521fa1cba51c29da55f3da0bf2b3cc6eeb197fe8
rensramos <rensramos17@gmail.com>
2017-09-12 05:12:56 +0800

    Update price

--- js/main.js
+++ js/main.js
@@ -120,20 +120,20 @@ $(function(){
 
         var total = 0;
         if (!model.complimentary) {
-            total += 20.00;
+            total += 300.00;
         }
         if (model.companions) {
-            total += 20.00 * Number(model.companions);
+            total += 200.00 * Number(model.companions);
         }
         if (model.courierRequested) {
-            total += 5.00;
+            total += 20.00;
         }
 
         if (total > 0) {
             if (model.paymentMethod == 'paypal-payment') {
-                total += 0.00;
+                total += 15.00;
             } else {
-                total += 0.00;
+                total += 8.00;
             }
         }
 
